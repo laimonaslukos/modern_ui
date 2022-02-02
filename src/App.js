0f7c09bc3e7e8ff298602a7ff33b1401a5f3ea45
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-02 12:04:42 +0200

    Create containers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Article from './components/article/Article';
-import Brand from './components/brand/Brand';
-import CTA from './components/cta/Cta';
-import Feature from './components/feature/Feature';
-import Navbar from './components/navbar/Navbar';
+import { Article, Brand, CTA, Feature, Navbar } from './components';
+
 
 const App = () => {
   return <div>
