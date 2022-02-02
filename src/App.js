6de336b9a3256bb8fb36e37bbfc7be394a05df32
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-02 11:46:34 +0200

    Import components to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/Cta';
+import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return <div>
