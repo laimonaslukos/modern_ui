451f08dd7d83b74e45c4af7a9de50b88ef1026de
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-06 15:12:51 +0200

    Import assets

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 
 
 const App = () => {
-  return <div className='App'>
-    <div className='gradient_bg'>
+  return <div className="App">
+    <div className="gradient__bg">
 <Navbar />
 <Header />
     </div>
