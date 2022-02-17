1c09522838f22f3106c4bd515a5614432cedcda7
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-17 10:19:36 +0200

    End

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { Brand, CTA, Navbar } from './components';
+import { CTA, Brand, Navbar } from './components';
+
 import './App.css';
 
-
-const App = () => {
-  return <div className="App">
+const App = () => (
+  <div className="App">
     <div className="gradient__bg">
-<Navbar />
-<Header />
+      <Navbar />
+      <Header />
     </div>
     <Brand />
     <WhatGPT3 />
@@ -17,8 +18,7 @@ const App = () => {
     <CTA />
     <Blog />
     <Footer />
-   
-  </div>;
-};
+  </div>
+);
 
 export default App;
