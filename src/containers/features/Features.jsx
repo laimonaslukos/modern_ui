6560b6764c81d3cc0181e49f59d6b778b237a0e9
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-17 11:05:48 +0200

    Finish

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -4,20 +4,20 @@ import './features.css';
 
 const featuresData = [
   {
-    title: 'Improving end distrusts instantly',
-    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
+    title: 'What is Lorem Ipsum?',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end disLorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis eros ac nisl tincidunt, ut facilisis ex molestie.trusts may instantly was household applauded.',
   },
   {
-    title: 'Become the tended active',
-    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+    title: 'What is Lorem Ipsum?',
+    text: 'ConsideNullam maximus arcu erat, eu lobortis neque auctor a. Aliquam ac mollis est.red sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
   },
   {
-    title: 'Message or am nothing',
-    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+    title: 'What is Lorem Ipsum?',
+    text: 'Praesent sodales rhoncus erat, sit amet varius ex euismod in. ',
   },
   {
-    title: 'Really boy law county',
-    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
+    title: 'What is Lorem Ipsum?',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sNullam maximus arcu erat, eu lobortis neque auctor a. Aliquam ac mollis est.ex are leave law built now. In built table in an rapid blush..',
   },
 ];
 
--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -5,16 +5,16 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="What is L L" text="Vivamus pharetra pulvinar ligula eu rutrum. Sed dictum aliquam molestie. Cras sollicitudin magna eu interdum venenatis. Mauris aliquam lectus vitae nunc iaculis interdum. Sed vitae consequat nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam vel maximus ipsum. " />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
       <p>Explore the Library</p>
     </div>
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Chatbots" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. " />
+      <Feature title="Knowledgebase" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. " />
+      <Feature title="Education" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. " />
     </div>
   </div>
 );
