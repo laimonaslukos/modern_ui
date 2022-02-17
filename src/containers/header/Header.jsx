7e706ee4f68d2b2c1472966e48141d09b536af15
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-17 10:25:19 +0200

    Text - header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,9 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">Let&apos;s Build Something amazing</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti eius iste hic, excepturi nostrum voluptate error ab aliquam, deserunt beatae sapiente. Voluptate officiis sunt perspiciatis, nulla ad corrupti cupiditate provident!
+      </p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
@@ -15,13 +16,13 @@ const Header = () => (
       </div>
 
       <div className="gpt3__header-content__people">
-        <img src={people} />
+        <img src={people} alt="people"/>
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
 
     <div className="gpt3__header-image">
-      <img src={ai} />
+      <img src={ai} alt="logo"/>
     </div>
   </div>
 );
