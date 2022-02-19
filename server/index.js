410ea8817e0836eded9216a45ceca4624ad21e53
„Laimonas <laimonas.lukosevicius@gmail.com>
2022-02-19 12:02:44 +0200

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -3,11 +3,16 @@ const express = require('express');
 const cors = require ('cors');
 const cookieParser = require ('cookie-parser');
 
+const mongoose = require( 'mongoose' )
+
+
 const PORT = process.env.PORT || 4000;
 const app = express()
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,
+      useUnifiedTopology: true})
 app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
   } catch (e) {
     console.log(e);
